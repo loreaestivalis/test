2a1565ca95a01a4a59588de323b47ba234971d72
loreaestivalis <anerol_115@hotmail.com>
2018-06-20 12:28:05 -0500

    New version2

---
+++ separacion.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import numpy as np
+from pandas import ExcelFile
+
+data = pd.read_csv('C:/Users/claudia11/Downloads/estaditica.csv', )
+#data = np.genfromtxt('C:/Users/claudia11/Downloads/estaditica.csv',delimiter=',',dtype=None)
+print data
+
---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import json
+
+
+
+
+
+filej = json.loads(open('C:/Users/claudia11/Downloads/csvjson2.json').read())
+print filej
